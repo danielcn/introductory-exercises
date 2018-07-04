f35209489073839ba8f4bca3d3fa7bf83a11d6de
Daniel Nascimento <dnascimen@MacBook-Pro-de-Daniel.local>
2018-07-04 18:43:53 -0300

    Add methods to return triangle and to repeat char in each line

--- src/com/twu/introductory/Triangle.java
+++ src/com/twu/introductory/Triangle.java
@@ -21,4 +21,21 @@ public class Triangle {
         }
         return verticalLine;
     }
+
+    public String drawTriangle(int sizeTriangle) {
+        String triangle = "";
+
+        for (int i = 1; i <= sizeTriangle; i++) {
+            triangle += reapeatChar("*", i) + "\n";
+        }
+        return triangle;
+    }
+
+    private String reapeatChar(String s, int size) {
+        StringBuilder r = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            r.append(s);
+        }
+        return r.toString();
+    }
 }
