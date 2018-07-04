e16cd30768f47bfa6cc930cacb40cdfd8ba15afc
Daniel Nascimento <dnascimen@MacBook-Pro-de-Daniel.local>
2018-07-04 16:06:01 -0300

    Adding file project structure

---
+++ src/com/twu/introductory/Main.java
@@ -0,0 +1,8 @@
+package com.twu.introductory;
+
+public class Main {
+
+    public static void  main(String args[]){
+        System.out.println("Teste");
+    }
+}
---
+++ test/com/twu/introductory/MainTest.java
@@ -0,0 +1,12 @@
+package com.twu.introductory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+    }
+}
